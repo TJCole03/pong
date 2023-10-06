fc2014d016819a4f9e0a1f17dfa748e3fab5bc20
TJCole03 <tjc6254@gmail.com>
2023-10-06 07:12:09 -0400

    ball and paddle created, movement logic implemented

--- Ball.js
+++ Ball.js
@@ -1,21 +1,73 @@
+const INITIAL_VELOCITY = .025;
+const VELOCITY_INCREASE = .00001
+
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
+        this.reset();
     }
 
     //helper functions to get the coordinates and position of ball
     get x() {
-        return getComputedStyle(this.ballElem).getPropertyValue("--x")
+        //taking the CSS x value of 50 and converting it into a JS number we can use
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
     }
-
+    //setter
     set x(value) {
         this.ballElem.style.setProperty("--x", value)
     }
-
-    update(delta) {
-        this.x = 5
+    get y() {
+        //taking the CSS y value of 50 and converting it into a JS number we can use
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    }
+    //setter
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value)
     }
 
+    rect() {
+        return this.ballElem.getBoundingClientRect()
+    }
+
+    reset() {
+        this.x = 50; 
+        this.y = 50;
+        this.direction = { x: 0 }
+        //in order to calculate our direction, we need to create a value for our x and y
+        while (    
+            Math.abs(this.direction.x) <= .2 ||
+            Math.abs(this.direction.x) >= .9
+        ) {
+            //gives us a random number betwwen 0 and 360 degrees; set in terms of radiance(radians?)
+            //with radians, we can use cosine and sine to determine the x and y direction
+            const heading = randomNumberBetween(0, 2 * Math.PI)
+            //say take in this direction and convet it into x and y position
+            //this will be our UNIT VECTOR for those positions
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading)}
+        }
+        this.velocity = INITIAL_VELOCITY
+    }
+
+    update(delta) {
+        this.x += this.direction.x * this.velocity * delta
+        this.y += this.direction.y * this.velocity * delta
+        this.velocity += VELOCITY_INCREASE * delta
+        const rect = this.rect()
+
+        if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+            this.direction.y *= -1
+        }
+        if (rect.right >= window.innerWidth || rect.left <= 0) {
+            this.direction.x *= -1
+        }
+    }
+}
+
+function randomNumberBetween(min, max) {
+    //gives us random number between 0 and 1, use max - min trick to make 
+    //sure it scales the value to be within our range
+    //and then add minumum on to make sure the minumum is the lowest number we can get
+    return Math.random() * (max - min) + min
 }
 
 //this element gets passed into the ball class so we can use it and interact with it
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Update Loop
 import Ball from './Ball.js'
+import Paddle from './Paddle.js'
 
 const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
 
 //can use this inside of our update loop
 //takes in time variable for how much timne has passed since start of program
@@ -17,13 +20,21 @@ function update(time) {
         ball.update(delta)
         //passing in delta because the due to how much 
         //the delta fluctuates in time
-        //important to use the delta to make sure all 
+        //important to use the delta to make sure all
         //game movements are based off the delta
+        computerPaddle.update(delta, ball.y)
         console.log(delta)
     }
 
     lastTime = time
     window.requestAnimationFrame(update)
 }
+
+document.addEventListener('mousemove', e => {
+    //setting position of player paddle
+    //this is a pixel value, so we need to convert to a percentage
+                            //gives value between 0 and 1, //multiply it by 100 to get the value bewtween 0 and 100 (%)
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
 
 window.requestAnimationFrame(update) 
